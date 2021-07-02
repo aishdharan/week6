ded4eb73bca7c2df76e9fe7165a0f00e66b435fb
aishdharan <aishwaryadharan@gmail.com>
2021-07-03 02:11:14 +0530

    generated DNA, RNA and Transcribed sequence

---
+++ week5/problem4.py
@@ -0,0 +1,43 @@
+import sys
+import random
+
+
+def main():
+    bases = ['A', 'T', 'G', 'C']
+    seq = random.choices(bases, k=100)
+    dna_seq = ''.join(seq)
+    print(dna_seq)
+    rna_seq = ""
+    for i in dna_seq:
+        if i == 'T':
+            rna_seq += 'U'
+        else:
+            rna_seq += i
+    print(rna_seq)
+    trans_seq = []
+    for k in rna_seq:
+        if k == "A":
+            trans_seq.append("U")
+        elif k == "U":
+            trans_seq.append("A")
+        elif k == "C":
+            trans_seq.append("G")
+        elif k == "G":
+            trans_seq.append("C")
+
+    #print(trans_seq)
+    trans_seq_join = ''.join(trans_seq)
+    print(trans_seq_join)
+    #  if 'T' in dna_seq:
+    #      rna_seq = dna_seq.replace('T', 'A')
+    #  if 'C' in dna_seq:
+    #      rna_seq = dna_seq.replace('C', 'G')
+    #  if 'G' in dna_seq:
+    #      rna_seq = dna_seq.replace('G', 'C')
+    # print(rna_seq)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
