3b68682d6691610909566e837eb1971fbf363265
aishdharan <aishwaryadharan@gmail.com>
2021-07-03 02:26:06 +0530

    generated DNA, RNA and Transcribed sequence; Translated RNA

--- week5/problem4.py
+++ week5/problem4.py
@@ -25,7 +25,7 @@ def main():
         elif k == "G":
             trans_seq.append("C")
 
-    #print(trans_seq)
+    # print(trans_seq)
     trans_seq_join = ''.join(trans_seq)
     print(f"Transcribed sequence = {trans_seq_join}")
     #  if 'T' in dna_seq:
@@ -36,7 +36,29 @@ def main():
     #      rna_seq = dna_seq.replace('G', 'C')
     # print(rna_seq)
 
-    return 0
+    rna_codon = {"UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
+                 "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
+                 "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
+                 "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
+                 "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
+                 "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
+                 "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
+                 "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
+                 "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
+                 "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+                 "UAA": "STOP", "CAA": "Q", "AAA": "K", "GAA": "E",
+                 "UAG": "STOP", "CAG": "Q", "AAG": "K", "GAG": "E",
+                 "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
+                 "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
+                 "UGA": "STOP", "CGA": "R", "AGA": "R", "GGA": "G",
+                 "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+                 }
+    translt_protein = ''
+    for i in range(0, len(trans_seq_join) - (3 + len(trans_seq_join) % 3), 3):
+        if rna_codon[trans_seq_join[i:i + 3]] == "STOP":
+            break
+        translt_protein += rna_codon[trans_seq_join[i:i + 3]]
+    print(f"Translated Protein = {translt_protein}")
 
 
 if __name__ == "__main__":
