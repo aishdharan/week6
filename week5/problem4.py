1f2a168f1a4c2e0d157e963f9702b33024794ff0
aishdharan <aishwaryadharan@gmail.com>
2021-07-10 01:53:24 +0530

    changed variables, added string method

--- week5/problem4.py
+++ week5/problem4.py
@@ -21,32 +21,33 @@ module by commenting things out. No need for this with git.
 
 def main():
     bases = ['A', 'T', 'G', 'C']
-    seq = random.choices(bases, k=100)
-    dna_seq = ''.join(seq)
-    print(f"DNA sequence = {dna_seq}")
-    rna_seq = ""
+    sequence = random.choices(bases, k=100)
+    dna_sequence = ''.join(sequence)
+    print(f"DNA sequence = {dna_sequence}")
     # todo: is there a string method that can replace the following 5 lines?
-    for i in dna_seq:
-        if i == 'T':
-            rna_seq += 'U'
-        else:
-            rna_seq += i
-    print(f"RNA sequence = {rna_seq}")
+    rna = dna_sequence.replace("T", "U")
+    # for i in dna_sequence:
+    #    if i == 'T':
+    #        rna += 'U'
+    #    else:
+    #        rna += i
+    print(f"RNA sequence = {rna}")
     # todo: which string method can perform transcription for you?
-    trans_seq = []
-    for k in rna_seq:
+    translated_sequence = []
+    # translated_sequence = rna.replace("A", "U").replace("U", "A").replace("C", "G").replace("G", "C")
+    for k in rna:
         if k == "A":
-            trans_seq.append("U")
+            translated_sequence.append("U")
         elif k == "U":
-            trans_seq.append("A")
+            translated_sequence.append("A")
         elif k == "C":
-            trans_seq.append("G")
+            translated_sequence.append("G")
         elif k == "G":
-            trans_seq.append("C")
+            translated_sequence.append("C")
 
     # print(trans_seq)
-    trans_seq_join = ''.join(trans_seq)
-    print(f"Transcribed sequence = {trans_seq_join}")
+    translated_joined = ''.join(translated_sequence)
+    print(f"Transcribed sequence = {translated_joined}")
     # fixme: the following lines should be in old commits and should not be here
     #  if 'T' in dna_seq:
     #      rna_seq = dna_seq.replace('T', 'A')
@@ -56,31 +57,31 @@ def main():
     #      rna_seq = dna_seq.replace('G', 'C')
     # print(rna_seq)
     # fixme: instead of 'rna_codon' consider 'genetic_code'
-    rna_codon = {"UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
-                 "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
-                 "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
-                 "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
-                 "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
-                 "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
-                 "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
-                 "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
-                 "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
-                 "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
-                 "UAA": "STOP", "CAA": "Q", "AAA": "K", "GAA": "E",
-                 "UAG": "STOP", "CAG": "Q", "AAG": "K", "GAG": "E",
-                 "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
-                 "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
-                 "UGA": "STOP", "CGA": "R", "AGA": "R", "GGA": "G",
-                 "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
-                 }
+    genetic_code = {"UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
+                    "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
+                    "UUA": "L", "CUA": "L", "AUA": "I", "GUA": "V",
+                    "UUG": "L", "CUG": "L", "AUG": "M", "GUG": "V",
+                    "UCU": "S", "CCU": "P", "ACU": "T", "GCU": "A",
+                    "UCC": "S", "CCC": "P", "ACC": "T", "GCC": "A",
+                    "UCA": "S", "CCA": "P", "ACA": "T", "GCA": "A",
+                    "UCG": "S", "CCG": "P", "ACG": "T", "GCG": "A",
+                    "UAU": "Y", "CAU": "H", "AAU": "N", "GAU": "D",
+                    "UAC": "Y", "CAC": "H", "AAC": "N", "GAC": "D",
+                    "UAA": "STOP", "CAA": "Q", "AAA": "K", "GAA": "E",
+                    "UAG": "STOP", "CAG": "Q", "AAG": "K", "GAG": "E",
+                    "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
+                    "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
+                    "UGA": "STOP", "CGA": "R", "AGA": "R", "GGA": "G",
+                    "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+                    }
     # fixme: it is better to have long variable names than ad hoc abbreviations; for example instead of 'translt_protein' better is
     #  'translated_protein' or simply 'protein'
-    translt_protein = ''
-    for i in range(0, len(trans_seq_join) - (3 + len(trans_seq_join) % 3), 3):
-        if rna_codon[trans_seq_join[i:i + 3]] == "STOP":
+    protein = ''
+    for i in range(0, len(translated_joined) - (3 + len(translated_joined) % 3), 3):
+        if genetic_code[translated_joined[i:i + 3]] == "STOP":
             break
-        translt_protein += rna_codon[trans_seq_join[i:i + 3]]
-    print(f"Translated Protein = {translt_protein}")
+        protein += genetic_code[translated_joined[i:i + 3]]
+    print(f"Translated Protein = {protein}")
 
     return 0
 
