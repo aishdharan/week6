757c249b28ff255cda462a34665e11cf6aebdd94
aishdharan <aishwaryadharan@gmail.com>
2021-07-12 02:36:34 +0530

    sum of numbers in a list

--- week6/problem0.py
+++ week6/problem0.py
@@ -1,3 +1,6 @@
+import sys
+
+
 # Find sum of numbers in a list
 
 
@@ -15,9 +18,13 @@ def sum_of_list(list, size):
         return list[size - 1] + sum_of_list(list, size - 1)
 
 
-list1 = [2, 4, 5, 6, 8, 9]
+def main():
+    list1 = [2, 4, 5, 6, 8, 9]
+    total_list = sum_of_list(list1, len(list1))
+    print(f"Sum of all elements in a given list= {total_list}")
+    return 0
 
-total_list = sum_of_list(list1, len(list1))
-print(f"Sum of all elements in a given list= {total_list}")
 
-# adding def main function not giving output for some reason
+if __name__ == "__main__":
+    sys.exit(main())
+
